5b9cb72f53a11ddddc55d30ce1e368900482e791
andre-cloud <55535285+andre-cloud@users.noreply.github.com>
2020-10-29 12:12:45 +0100

    try aws

--- newsite/settings.py
+++ newsite/settings.py
@@ -138,11 +138,7 @@ try:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'ascelpio-site'
 
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-    STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
     django_heroku.settings(locals())
 except ImportError as e:
